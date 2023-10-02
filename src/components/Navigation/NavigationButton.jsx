b0187dd606874401d37f8d577d2211fd27bc2ed9
Omribak <97708552+Omribak@users.noreply.github.com>
2023-10-02 14:05:48 +0300

    New Resume

--- src/components/Navigation/NavigationButton.jsx
+++ src/components/Navigation/NavigationButton.jsx
@@ -1,7 +1,7 @@
 function NavigationButton() {
   return (
     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-      <a href="src\portfolio\Resume2.pdf" download>
+      <a href="src\portfolio\OmriBakalResume.pdf" download>
         Resume
       </a>
     </button>
